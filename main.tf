1e70a04f296c907494c30c14e791f8f7da2dfbc8
clmathe-AIC <chris.matheny@ascendantinnovations.com>
2025-08-25 21:31:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,38 +5,38 @@ region = "us-east-1" # Specify your desired AWS region
 
 # Data source to retrieve the latest Ubuntu AMI
 
-data "aws_ami" "latest_RHEL8_ami" {
+data "aws_ami" "latest_RHEL9_ami" {
 most_recent = true
-owners = ["966922279128"]
+owners = [904233107370""]
 filter {
 name = "name"
-values = ["eVo_AMI_RHEL8-*"]
+values = ["AMI_RHEL9-*"]
 }
 }
 
-data "aws_ami" "latest_RHEL9_ami" {
+data "aws_ami" "latest_RHEL10_ami" {
 most_recent = true
-owners = ["966922279128"]
+owners = ["904233107370"]
 filter {
 name = "name"
-values = ["eVo_AMI_RHEL9-*"]
+values = ["AMI_RHEL10-*"]
 }
 }
 
 resource "aws_instance" "test_vm" {
 
 ami = data.aws_ami.latest_RHEL9_ami.id
-key_name = "clmathe-evo"
+key_name = "ec2-test"
 root_block_device {
 volume_size = 50 # Sets the root volume size to 50 GiB
 volume_type = "gp3" # Optional: specify volume type (e.g., gp2, gp3)
 delete_on_termination = true # Optional: whether to delete volume on instance termination
 
 }
-vpc_security_group_ids = ["sg-0013d33699e7e69cc"]
+vpc_security_group_ids = ["sg-0b572afa6c7ba24d4"]
 instance_type = "t2.micro"
 #associate_public_ip_address = true
-subnet_id = "subnet-087dd641cea6d393f"
+subnet_id = "subnet-02914675f4e13b0d4"
 tags = {
 Name = "test terraform",
 }
@@ -45,7 +45,7 @@ Name = "test terraform",
 resource "aws_security_group" "terraform_server_sg" {
 name = "terraform_server_security_group"
 description = "Allow inbound HTTP and SSH traffic"
-vpc_id = "vpc-0eda63b3db7b50500" # Replace with your VPC ID or reference
+vpc_id = "vpc-0963a3d1f78e56fc0" # Replace with your VPC ID or reference
 
 ingress {
 from_port = 80
